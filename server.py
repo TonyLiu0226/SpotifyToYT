9f48f75d56b04e606a5d50e2c2065099d2ef007a
Tony Liu <tl0226yn@gmail.com>
2024-07-30 23:26:07 -0700

    working spotify to yt pipeline for individual playlists

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from typing import Union
 from aiohttp import request, ClientSession
 from fastapi import FastAPI
 from fastapi.concurrency import asynccontextmanager
+from ytmusicapi import YTMusic
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -20,8 +21,31 @@ async def lifespan(app: FastAPI):
     await session.close()
 
 app = FastAPI(lifespan=lifespan)
+ytmusic = YTMusic("oauth.json")
 session = None
 token = None
+
+async def createPlayList(name):
+    try:
+        result = ytmusic.create_playlist(title=name, description="")
+        return result
+    except Exception as e:
+        print(e)
+
+async def searchSongByNameAndArtist(name, artist):
+    try:
+        result = ytmusic.search(query=f"${name} by ${artist}", filter="songs")
+        print(result[0])
+        return result[0]
+    except Exception as e:
+        print(e)
+
+async def addSongToPlaylist(playlistId, videoId):
+    try:
+        result = ytmusic.add_playlist_items(playlistId=playlistId, videoIds=[videoId])
+        print(result)
+    except Exception as e:
+        print(e)
 
 @app.get("/")
 def read_root():
@@ -40,15 +64,15 @@ async def playlist( id: str):
     }
     try:
         if id:
-            async with session.get(f"https://api.spotify.com/v1/playlists/{id}?fields=tracks.items(track(name, album(artists)))", headers=headers) as response:
+            async with session.get(f"https://api.spotify.com/v1/playlists/{id}?fields=name,tracks.items(track(name, album(artists)))", headers=headers) as response:
                 if response.status == 200:
                     #must return name of playlist and its tracks (name, artists list)
                     #currently only returns 100 songs for some reason
                     songs = {}
                     result = await response.json()
+                    name = result["name"]
 
                     for i in range (len(result['tracks']['items'])):
-                        print(result['tracks']['items'][i])
                         songs[(result['tracks']['items'][i]['track']['name'])] = []
 
                         localArtists = []
@@ -56,6 +80,13 @@ async def playlist( id: str):
                             localArtists.append(artist['name'])
 
                         songs[(result['tracks']['items'][i]['track']['name'])].append(localArtists)  
+
+                    playlistId = await createPlayList(name)
+
+                    for song in songs.keys():
+                        result = await searchSongByNameAndArtist(song, songs[song])
+                        final = await addSongToPlaylist(playlistId, result['videoId'])
+                        print(final)
 
                     return (songs)
                 else:
@@ -79,8 +110,6 @@ async def authenticate(session):
         print(response.status)
         cookie = await response.json()
         return cookie['access_token']
-
-
 
         
 # # thread to regenerate tokens every 30 minutes (implement when using web app)
